29256b0cec108f9ebbff3a99d2788f580985da08
AkhilParim <akhil.parim@gmail.com>
2024-02-20 16:53:18 -0600

    fonts and experience

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Portfolio';
+  constructor(private service: AppService) { }
+
+  experiences = this.service.experiences;
+
 }
